218228b3447a26fb9cd0ea34ccaf22537cbd7835
Sachinnandu <727821tuit128@skct.edu.in>
2024-03-23 21:30:25 +0530

    Day 9

--- backend/demo/src/main/java/com/example/demo/Controller/AuthenticationController.java
+++ backend/demo/src/main/java/com/example/demo/Controller/AuthenticationController.java
@@ -1,42 +1,47 @@
-package com.tech.course_mania.controller;
+package com.abhishek.giftcraft.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tech.course_mania.dto.AuthRequest;
-import com.tech.course_mania.service.JwtService;
+import com.abhishek.giftcraft.dto.Request.AuthenticationRequest;
+import com.abhishek.giftcraft.dto.Request.RegisterRequest;
+import com.abhishek.giftcraft.dto.Response.AuthenticationResponse;
+import com.abhishek.giftcraft.entity.UserRegister;
+import com.abhishek.giftcraft.repository.UserRegitserRepository;
+import com.abhishek.giftcraft.service.AuthenticationService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")	
+@RequiredArgsConstructor
 public class AuthenticationController {
-
     @Autowired
-    private JwtService jwtService;
+	private UserRegitserRepository userrepo;
+	private final AuthenticationService service;
+	@PostMapping("/register")
+	 public ResponseEntity<String> register(
+		      @RequestBody RegisterRequest request
+		  ) {
+		service.register(request);
+		    return ResponseEntity.ok("Registered Successfully");
+		  }
+	@GetMapping("/getregister")
+	public List<UserRegister> getregister()
+	{
+		return userrepo.findAll();
+		
+	}
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @GetMapping("/home")
-    public String Home() {
-        return "Initial Render Page.!";
-    }
-
-    @PostMapping("/authenticate")
-    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
-        if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getEmail());
-        } else {
-            throw new UsernameNotFoundException("Invalid user request!");
-        }
-    }
+		  @PostMapping("/authentication")
+		  public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request)
+		  {
+			return ResponseEntity.ok(service.authenticate(request));
+			  
+		  }
 }
